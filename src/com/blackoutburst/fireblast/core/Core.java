fcf8f5da37d35b3e351d5806615c15179ff42bb8
Blackoutburst <blackoutburst@hotmail.com>
2022-02-08 15:14:41 +0100

    Removed duplicated entries for time in scoreboard

--- src/com/blackoutburst/fireblast/core/Core.java
+++ src/com/blackoutburst/fireblast/core/Core.java
@@ -91,7 +91,7 @@ public class Core {
         final String time = String.format("%d:%02d", minutes, seconds);
 
         for (BlastPlayer p : Main.players) {
-            p.getBoard().set(13, "Time: §a"+time);
+            p.getBoard().set(14, "Time: §a"+time);
         }
     }
 
